933b455e1366066bd457b2d316fcd250af9da6da
Neel Patel <npate306@illinois.edu>
2017-10-02 21:45:08 -0500

    Added to the cuisines page

---
+++ app/src/main/java/com/example/neelpatel/foodapp/Cuisines.java
@@ -0,0 +1,18 @@
+package com.example.neelpatel.foodapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Neel Patel on 10/2/2017.
+ */
+
+public class Cuisines extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cuisines);
+    }
+}
--- app/src/main/java/com/example/neelpatel/foodapp/MainActivity.java
+++ app/src/main/java/com/example/neelpatel/foodapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.neelpatel.foodapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button cuisines;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cuisines = (Button) findViewById(R.id.cuisines);
+        cuisines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Cuisines.class);
+                startActivity(i);
+            }
+        });
     }
 }
